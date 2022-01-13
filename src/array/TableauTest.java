c5dc7559640a8dcd001120c208cf3579adcc9778
alexandre-vagnair <alexandre.vagnair@easy.movie>
2022-01-13 17:58:09 +0100

    add work on equal overwrite & reorder class in package

---
+++ src/array/TableauTest.java
@@ -0,0 +1,32 @@
+package array;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TableauTest {
+
+    @Test
+    @DisplayName("Test adding value in array")
+    void addValue() {
+        Tableau t = new Tableau(10);
+        assertEquals(10, t.values.size());
+        assertTrue(t.addValue(10));
+        t.printValues();
+    }
+
+    @Test
+    @DisplayName("Test Sort array")
+    void sortByDir() {
+        Tableau t = new Tableau(20);
+        t.sortByDir(null);
+        t.printValues();
+        t.sortByDir("desc");
+        t.printValues();
+        t.sortByDir("asc");
+        t.printValues();
+
+        assertTrue(true);
+    }
+}
---
+++ src/equals/Movie.java
@@ -0,0 +1,110 @@
+package equals;
+
+import java.util.Comparator;
+import java.util.Date;
+
+public class Movie implements Comparable<Movie> {
+    private String title;
+    private Date releaseDate;
+    private Integer ranking;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Integer getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(Integer ranking) {
+        this.ranking = ranking;
+    }
+
+    public Movie(String title, Date releaseDate, Integer ranking) {
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.ranking = ranking;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + getTitle() + " Ranking : " + getRanking();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Movie m = (Movie) obj;
+        if (title == null) {
+            if(m.title != null) {
+                return false;
+            }
+        }else if (!title.equals(m.title)) {
+            return false;
+        }
+
+        if (releaseDate == null) {
+            if (m.releaseDate != null) {
+                return false;
+            }
+        } else if (!releaseDate.equals(m.releaseDate)) {
+            return false;
+        }
+
+        if (ranking == null) {
+            if (m.ranking != null) {
+                return false;
+            }
+        } else if (!ranking.equals(m.ranking)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int res = 1;
+        int cote = 42;
+
+        res = res * cote + (title != null ? title.hashCode() : 0 );
+        res = res * cote + (releaseDate != null ? releaseDate.hashCode() : 0 );
+        res = res * cote + (ranking != null ? ranking.hashCode() : 0 );
+
+        return res;
+    }
+
+    @Override
+    public int compareTo(Movie m) {
+        if (m.ranking > this.ranking) {
+            return 1;
+        }else if (this.ranking > m.ranking) {
+            return -1;
+        }
+
+        return 0;
+    }
+}
